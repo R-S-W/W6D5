6fbc70f998b6245445ee5769cbeeff403ee8f86b
Daniel Bloncourt <dblonc@aol.com>
2021-02-05 17:03:36 -0500

    Completed phase 1

--- ninety_nine__cats/app/controllers/cats_controller.rb
+++ ninety_nine__cats/app/controllers/cats_controller.rb
@@ -15,5 +15,38 @@ class CatsController < ApplicationController
         render :new
     end
 
+    def edit
+        @cat = Cat.find(params[:id])
+        render :edit
+    end
+
+    def create
+        cat = Cat.new(cat_params)
+        if cat.save
+            redirect_to cat_url(cat)
+        else
+            render json: cat.errors.full_messages, status:422
+        end
+    end
+
+    def update
+        @cat = Cat.find(params[:id])
+
+        if @cat.update(cat_params)
+            redirect_to cat_url(@cat)
+        else
+            render json: cat.errors.full_messages, status:422
+        end
+    end
+
+
+
+
+    private
+
+    def cat_params
+        params.require(:cat).permit(:name, :sex, :birth_date, :color, :description)
+    end
+
 
 end
